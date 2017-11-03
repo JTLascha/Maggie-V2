238ad7097bf90a57cd8469c326d0b922bcfdf54f
Justin Tomlinson <justintomlinsonn@gmail.com>
2017-11-02 22:57:16 -0400

    Added battery bot and saw
    I have added the prefabs for both the saw and the battery bot. Since
    they currently use the same script, I have included that as well. The
    sprite probably won't render since I used some art since I made myself.
    The script is very similar to the simple moving platform script with the
    addition of a function that resets the player when they trigger the
    collision. The trigger function is very easy to change if we would
    rather use checkpoints or have damage. Currently, all they do is just
    move back and forth and if you touch them it resets the player. I
    figured since we wanted them to be like a Goomba, this would fit that. I
    still would like to add some extra functionality and interaction with
    the magnet gun though. Hopefully I will be able to do that before this
    iteration's end.

---
+++ scripts/BatteryBot.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteryBot : MonoBehaviour {
+
+    //A lot of this code comes straight from the simple moving platform script.
+    //It also causes the level to be reset when the player collides with the 
+    //bot. Once we implement damage or checkpoints, it can be updated to properly
+    //support that.
+
+    public Vector3 pos1;
+    public Vector3 pos2;
+    public float speed = 2f;
+    float timer = 0f;
+    bool direction = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos1 = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GetComponent<Transform>().position == pos1)
+        {
+            direction = true;
+        }
+
+        if (GetComponent<Transform>().position == pos2)
+        {
+            direction = false;
+        }
+
+        if (direction == true)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+
+        GetComponent<Transform>().position = Vector3.Lerp(pos1, pos2, timer * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel(Application.loadedLevelName);
+        }
+    }
+}
