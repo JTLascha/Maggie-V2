a042a29216f242dafb200a7dfcaa309448e91f4b
JTLascha <296577@gmail.com>
2017-11-04 23:43:58 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/JTLascha/Maggie-V2""
    This reverts commit 10cb8790422eeb6e6579451743891abd22262343.

---
+++ scripts/NewMovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿//Cody O'Connor
+//
+//NewMovingPlatform.cs
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovingPlatform : MonoBehaviour {
+
+    public GameObject platform;
+    public float MoveSpeed;
+    public Transform CurrentPos;    //current position
+    public Transform[] points;      //allows for multiple points on the level for the platform to move to.
+    public int PointSelection;
+
+	// Use this for initialization
+	void Start () {
+        CurrentPos = points[PointSelection];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, CurrentPos.position, Time.deltaTime * MoveSpeed);    //platform will move to next point
+
+        if(platform.transform.position == CurrentPos.position)      //when it has reached the point it will either go to the next point or back to the beginning to the first point so
+        {                                                           //platform will move in an infinite loop
+            PointSelection++;
+            if (PointSelection == points.Length)
+            {
+                PointSelection = 0;
+            }
+            CurrentPos = points[PointSelection];
+        }
+     
+	}
+}
