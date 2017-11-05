1d0d8318df35b526e457a425b4a8c18de2b51cf6
dillonpren <32419297+dillonpren@users.noreply.github.com>
2017-11-05 09:29:33 -0500

    Script Prettier/Rapid Fire/Collision Detect
    -LaserGun_Script was cleaned up and made to shoot continuously without input
    
    -Laser was cleaned up and given collision detection (destructs on hit)

--- scripts/Laser.cs
+++ scripts/Laser.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour {
+public class Laser : MonoBehaviour {
 
     public float speed;
+    public bool bullet = true;
     private Rigidbody rb;
-
+    
+   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.right * speed;
     }
 
+    private void OnCollisionEnter()         //desruct on collision
+    {
+        Destroy(gameObject);
+    }
     //on collision check if object is damageable
     //if damageable==true
     //Damage funct
--- scripts/LaserGun_Script.cs
+++ scripts/LaserGun_Script.cs
@@ -2,32 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun_Script : MonoBehaviour {
+public class LaserGun_Script : MonoBehaviour {
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
 
-    private float nextFire;
+    private float nextFire=0;
 
    
     void Update()
     {
+        float angle;
+        Vector3 mousePos;
+        Vector3 objectPos;
 
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        //constant shooting lasers
+        if (Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
 
-        //rotation
-        Vector3 mousePos = Input.mousePosition;
+        //rotation of laser gun
+        mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
 
-        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        objectPos = Camera.main.WorldToScreenPoint(transform.position);
         mousePos.x = mousePos.x - objectPos.x;
         mousePos.y = mousePos.y - objectPos.y;
 
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
     }
 }
