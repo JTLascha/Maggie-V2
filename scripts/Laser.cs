df2892c881a073e002892b1c6e9f32d36e3187b9
dillonpren <32419297+dillonpren@users.noreply.github.com>
2017-11-05 00:21:42 -0400

    Added the scripts for laser gun/shooter + laser
    -Gun rotates with mouse and shoots on left click (will be changed to constant shooting)
    -Lasers travel straight w collision possible (will add function to check damageable collisions)

---
+++ scripts/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * speed;
+    }
+
+    //on collision check if object is damageable
+    //if damageable==true
+    //Damage funct
+}
---
+++ scripts/LaserGun_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Script : MonoBehaviour {
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+   
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+        //rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+    }
+}
