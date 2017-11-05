7791d1eaef306a96a4abf9d301e6b603dcb97896
dillonpren <32419297+dillonpren@users.noreply.github.com>
2017-11-05 09:30:49 -0500

    Created Boundary for laser or enemies
    Currently only lasers are destroyed after passing boundary but enemies can be when moved offscreen

---
+++ scripts/DestructionBoundary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionBoundary : MonoBehaviour {
+
+    public bool bullet;
+                                                //destroys laser bullets after boundary
+    void OnTriggerExit(Collider other)
+    {
+        Laser controlscript = other.GetComponent<Laser>();
+        bullet = controlscript.bullet;
+        if (bullet==true)
+            Destroy(other.gameObject);
+    }
+}
