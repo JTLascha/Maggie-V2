135ef4a06a5023a52cf575e56cd203b169e6d332
Homer <hmcmillanthethird@gmail.com>
2017-11-19 15:26:42 -0500

    Bomb Bot improvements
    This commit makes Bomb Bot a damageable, makes Bomb Bot explode on a collision (calling the damage() method for anything caught by it), and fixes the bug which caused Bomb Bot's to detect Maggies' Magnet through walls. This last change required adding a layer to all non-metal platforms called "terrain"

--- scripts/BombBot.cs
+++ scripts/BombBot.cs
@@ -1,34 +1,55 @@
-﻿using System.Collections;
+﻿/* File:	BombBot.cs
+ * Author:	Homer McMillan
+ * 
+ * Notes:	This script defines the behavior of the Bomb Bot enemy.
+ * 
+ * TODO:	Add further interactions with Maggie's magnet (repulsive/attractive force)
+ */
+
+
+
+
+using System.Collections;
 using UnityEngine;
 
-public class BombBot : MonoBehaviour {
+public class BombBot : damageable {
 	public GameObject maggie;
 	public float chase_speed = 10f;
 	public bool locked_on;
+	private float explosion_radius = 3f;
 
 	void Start () {
 		locked_on = false;
 	}
-	
 
-	void Update () {
-		if (locked_on) { Chase (); }
+	void FixedUpdate() {
+		if (locked_on) {
+			Chase ();
+		}
 	}
 
 	void Chase() {
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 
-		if ((maggie.transform.position - transform.position).magnitude < 1f) {
-			// stops moving and chasing. Only for testing right now
-			locked_on = false;
-			rb.velocity = new Vector2(0,0);
-
-		} else {
-			rb.velocity = chase_speed * (Vector2)(maggie.transform.position - transform.position).normalized;
-		}
+		rb.velocity = chase_speed * (Vector2)(maggie.transform.position - transform.position).normalized;
 	}
 
 	void LockOn() {
 		locked_on = true;
+	}
+
+	private void OnCollisionEnter2D () {
+		locked_on = false;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosion_radius);
+		for (int i = 0; i < colliders.Length; i++) {
+			damageable target = colliders [i].GetComponent<damageable>();
+			if (target) {
+				target.damage ();
+			}
+		}
+	}
+
+	override public void damage() {
+		Destroy (gameObject);
 	}
 }	
