400df19dc597fc0fbb54bd5b151b62d8e09f0b9c
cjo15b <32645293+cjo15b@users.noreply.github.com>
2017-12-01 23:31:40 -0500

    MainMenu updates
    added scripts for the main menu buttons
    updated MainMenu prefab
    
    cody O'Connor

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿//MainMenu.cs
+//Cody O'Connor
+//group 15 project, maggie the magbot
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame() {
+        SceneManager.LoadScene("Level1");
+	}
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+
+	
+	
+}
