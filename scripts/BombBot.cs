9910808bf084fe74f33a6c3df7e7d8b88af3f285
Homer <hmcmillanthethird@gmail.com>
2017-12-02 11:03:56 -0500

    Add patrolling to Bomb Bot

--- scripts/BombBot.cs
+++ scripts/BombBot.cs
@@ -15,16 +15,25 @@ using UnityEngine;
 public class BombBot : damageable {
 	public GameObject maggie;
 	public float chase_speed = 10f;
+	public float patrol_speed = 5f;
+	public Vector3 patrol_start;
+	public Vector3 current_destination;
+	public Vector3 patrol_end;
 	public bool locked_on;
-	private float explosion_radius = 3f;
+	public bool patrolling = true;
+	private float explosion_radius = 3f;	
 
 	void Start () {
 		locked_on = false;
+		patrol_start = transform.position;
+		current_destination = patrol_end;
 	}
 
 	void FixedUpdate() {
 		if (locked_on) {
 			Chase ();
+		} else if (patrolling) {
+			Patrol ();
 		}
 	}
 
@@ -34,8 +43,21 @@ public class BombBot : damageable {
 		rb.velocity = chase_speed * (Vector2)(maggie.transform.position - transform.position).normalized;
 	}
 
+	void Patrol() {
+		if (transform.position == current_destination && current_destination == patrol_end) {
+			current_destination = patrol_start;
+			transform.position = Vector2.MoveTowards (transform.position, current_destination, Time.deltaTime * patrol_speed);
+		} else if (transform.position == current_destination) {
+			current_destination = patrol_end;
+			transform.position = Vector2.MoveTowards (transform.position, current_destination, Time.deltaTime * patrol_speed);
+		} else {
+			transform.position = Vector2.MoveTowards (transform.position, current_destination, Time.deltaTime * patrol_speed);
+		}
+	}
+
 	void LockOn() {
 		locked_on = true;
+		patrolling = false;
 	}
 
 	private void OnCollisionEnter2D () {
